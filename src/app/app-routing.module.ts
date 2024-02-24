f540302e4a8967d81036cb2d92a0ef975605e938
gy10jv9 <vincentm.work.2023@gmail.com>
2024-02-24 11:41:46 +0800

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "todo",
+        component: ToDoComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
