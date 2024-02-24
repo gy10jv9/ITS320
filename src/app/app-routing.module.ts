251ff4e2e3da94542a35a7e7941bd6496ebe2438
gy10jv9 <vincentm.work.2023@gmail.com>
2024-02-24 14:59:32 +0800

    added hero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 import { ToDoComponent } from './to-do/to-do.component';
 
 const routes: Routes = [
     {
         path: "todo",
         component: ToDoComponent
+    },
+    {
+        path: "hero",
+        component: HeroComponent
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoComponent
+    ToDoComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
