2ab8ac9af0bb938c9859f223497f322e613b428b
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-03-06 05:24:12 +0800

    added task list

--- src/app/todo-editor/todo-editor.component.ts
+++ src/app/todo-editor/todo-editor.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-todo-editor',
-  templateUrl: './todo-editor.component.html',
-  styleUrls: ['./todo-editor.component.css']
+	selector: 'app-todo-editor',
+	templateUrl: './todo-editor.component.html',
+	styleUrls: ['./todo-editor.component.css']
 })
 export class TodoEditorComponent {
-  todo_form = new FormGroup({
-    task: new FormControl(''),
-    status: new FormControl('pending')
-  });
+	task = new FormControl('')
+	task_list: string[] = ["test 1", "test 2", "test 3"]
 
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.todo_form.value);
-  }
+	onSubmit = () => {
+		this.task_list = [...this.task_list, this.task.value!];
+	}
 }
 
