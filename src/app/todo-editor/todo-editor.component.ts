99d881c4225ad525fbb3d950a2f4a4ae1fc57d82
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-03-06 21:50:22 +0800

    added validator

--- src/app/todo-editor/todo-editor.component.ts
+++ src/app/todo-editor/todo-editor.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-todo-editor',
 	templateUrl: './todo-editor.component.html',
 	styleUrls: ['./todo-editor.component.css']
 })
+
 export class TodoEditorComponent {
-	task = new FormControl('')
-	task_list: string[] = ["test 1", "test 2", "test 3"]
+	task = new FormControl('', Validators.required)
+	task_list: string[] = []
 
 	onSubmit = () => {
-		this.task_list = [...this.task_list, this.task.value!];
+		console.log("submit button is working")
+
+		this.task_list = [...this.task_list, this.task.value!]
+		this.task.reset()
+	}
+
+
+	/* pabalo sng status kng valid or invalid */
+	validation = "invalid!"
+	ngOnInit(): void {
+		this.task.statusChanges.subscribe((status) => {
+			this.validation = status === "VALID" ? "ok" : "invalid!"
+		})
 	}
 }
 
