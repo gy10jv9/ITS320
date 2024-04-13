2d55dc72e5959cf4f7f64d346ad1688684386cd3
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2024-04-13 11:56:32 +0800

    fixed taskRouter error

---
+++ backend/routes/task.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const createTask = require("../controller/task");
+const taskRouter = express.Router();
+
+taskRouter.post("/tasks", async (req, res) => {
+    // const payload = req.body
+    // const newPost = await createTask(payload)
+    // return res.json(newPost)
+})
+
+module.exports = taskRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express'); 
 const { PrismaClient } = require('@prisma/client');
-const { taskRouter } = require('./route/task');
+const taskRouter = require('./routes/task');
 
 const prisma = new PrismaClient();
 const app = express(); 
 
 app.use(express.json());
+app.use(taskRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => { 
